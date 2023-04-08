89eaa698214c53381fa83382b3edae3c9c29acae
Marko-Stojanovic <98714766+Marko-Stojanovic@users.noreply.github.com>
2023-04-08 17:00:38 -0400

    Fixed click propagation in mobile nav

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ $(document).ready(function() {
     // Load nav into each page
     $('nav#desktop').load('/components/nav.html');
     $('nav#mobile').load('/components/nav_mobile.html', function() {
+
+        // Show mobile-friendly nav when clicking menu button
         $("nav#mobile img#menu").click(function() {
             $("nav#mobile ul").css("display", "flex");
         });
 
-        $("nav#mobile ul").click(function() {
-            $("nav#mobile ul").css("display", "none");
+        // Hide mobile-friendly nav when clicking nav background
+        $("nav#mobile ul").click(function(e) {
+            if(e.target == this) {
+                $("nav#mobile ul").css("display", "none");
+            }
         });
     });
 });
