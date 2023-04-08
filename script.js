a6f31b1f5bf051b762e98c33624ee3265d725155
Marko-Stojanovic <98714766+Marko-Stojanovic@users.noreply.github.com>
2023-04-08 17:21:02 -0400

    Added nav highlighting for current page

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
     $('nav#desktop').load('/components/nav.html');
     $('nav#mobile').load('/components/nav_mobile.html', function() {
 
+        // Change nav link color to blue for current page
+        if (window.location.pathname == '/') { $("nav ul li:nth-child(1) a").css('color', 'var(--color-blue-light)'); }
+        if (window.location.pathname == '/events.html') { $("nav ul li:nth-child(2) a").css('color', 'var(--color-blue-light)'); }
+        if (window.location.pathname == '/about.html') { $("nav ul li:nth-child(3) a").css('color', 'var(--color-blue-light)'); }
+
         // Show mobile-friendly nav when clicking menu button
         $("nav#mobile img#menu").click(function() {
             $("nav#mobile ul").css("display", "flex");
